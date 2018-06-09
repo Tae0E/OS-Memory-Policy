7a47afab94d34a1a3a41e82bbf08c4a597d9180d
Tae0E <somuch0706@naver.com>
2018-06-09 10:21:37 +0900

    Add Simulator

---
+++ Memory_Policy_Simulator/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Memory_Policy_Simulator
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        PictureBox pbPlaceHolder;
+        Bitmap bResultImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.pbPlaceHolder = new PictureBox();
+            this.bResultImage = new Bitmap(2048, 2048);
+            this.pbPlaceHolder.Size = new Size(2048, 2048);
+            g = Graphics.FromImage(this.bResultImage);
+            pbPlaceHolder.Image = this.bResultImage;
+            this.pImage.Controls.Add(this.pbPlaceHolder);
+        }
+
+        private void DrawBase(LRU core, int windowSize, int dataLength, int[] numb)
+        {
+            /* parse window */
+            var psudoQueue = new List<char>();
+
+            g.Clear(Color.Black);
+
+            for ( int i = 0; i < dataLength; i++ ) // length
+            {
+                int psudoCursor = core.pageHistory[i].loc;
+                char data = core.pageHistory[i].data;
+                Page.STATUS status = core.pageHistory[i].status;
+
+                switch ( status )
+                {
+                    case Page.STATUS.PAGEFAULT:
+                        psudoQueue.Add(data);
+                        break;
+                    case Page.STATUS.MIGRATION:
+                        psudoQueue.RemoveAt(numb[i]);
+                        psudoQueue.Insert(numb[i], data);
+                        break;
+                }
+
+                for ( int j = 0; j <= windowSize; j++) // height - STEP
+                {
+                    if (j == 0)
+                    {
+                        DrawGridText(i, j, data);
+                    }
+                    else
+                    {
+                        DrawGrid(i, j);
+                    }
+                }
+
+                DrawGridHighlight(i, psudoCursor, status);
+                int depth = 1;
+
+                foreach ( char t in psudoQueue )
+                {
+                    DrawGridText(i, depth++, t);
+                }
+            }
+        }
+        private void DrawBase(LFU core, int windowSize, int dataLength, int[] numb)
+        {
+            /* parse window */
+            var psudoQueue = new List<char>();
+
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < dataLength; i++) // length
+            {
+                int psudoCursor = core.pageHistory[i].loc;
+                char data = core.pageHistory[i].data;
+                Page.STATUS status = core.pageHistory[i].status;
+
+                switch (status)
+                {
+                    case Page.STATUS.PAGEFAULT:
+                        psudoQueue.Add(data);
+                        break;
+                    case Page.STATUS.MIGRATION:
+                        psudoQueue.RemoveAt(numb[i]);
+                        psudoQueue.Insert(numb[i], data);
+                        break;
+                }
+
+                for (int j = 0; j <= windowSize; j++) // height - STEP
+                {
+                    if (j == 0)
+                    {
+                        DrawGridText(i, j, data);
+                    }
+                    else
+                    {
+                        DrawGrid(i, j);
+                    }
+                }
+
+                DrawGridHighlight(i, psudoCursor, status);
+                int depth = 1;
+
+                foreach (char t in psudoQueue)
+                {
+                    DrawGridText(i, depth++, t);
+                }
+            }
+        }
+        private void DrawBase(MFU core, int windowSize, int dataLength, int[] numb)
+        {
+            /* parse window */
+            var psudoQueue = new List<char>();
+
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < dataLength; i++) // length
+            {
+                int psudoCursor = core.pageHistory[i].loc;
+                char data = core.pageHistory[i].data;
+                Page.STATUS status = core.pageHistory[i].status;
+
+                switch (status)
+                {
+                    case Page.STATUS.PAGEFAULT:
+                        psudoQueue.Add(data);
+                        break;
+                    case Page.STATUS.MIGRATION:
+                        psudoQueue.RemoveAt(numb[i]);
+                        psudoQueue.Insert(numb[i], data);
+                        break;
+                }
+
+                for (int j = 0; j <= windowSize; j++) // height - STEP
+                {
+                    if (j == 0)
+                    {
+                        DrawGridText(i, j, data);
+                    }
+                    else
+                    {
+                        DrawGrid(i, j);
+                    }
+                }
+
+                DrawGridHighlight(i, psudoCursor, status);
+                int depth = 1;
+
+                foreach (char t in psudoQueue)
+                {
+                    DrawGridText(i, depth++, t);
+                }
+            }
+        }
+
+        private void DrawGrid(int x, int y)
+        {
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+
+            g.DrawRectangle(new Pen(Color.White), new Rectangle(
+                gridBaseX + (x * gridSpace),
+                gridBaseY,
+                gridSize,
+                gridSize
+                ));
+        }
+
+        private void DrawGridHighlight(int x, int y, Page.STATUS status)
+        {
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+
+            SolidBrush highlighter = new SolidBrush(Color.LimeGreen);
+
+                switch (status)
+                {
+                    case Page.STATUS.HIT:
+                        break;
+                    case Page.STATUS.MIGRATION:
+                        highlighter.Color = Color.Purple;
+                        break;
+                    case Page.STATUS.PAGEFAULT:
+                        highlighter.Color = Color.Red;
+                        break;
+                }
+           
+            g.FillRectangle(highlighter, new Rectangle(
+                gridBaseX + (x * gridSpace),
+                gridBaseY,
+                gridSize,
+                gridSize
+                ));
+        }
+
+        private void DrawGridText(int x, int y, char value)
+        {
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+
+            g.DrawString(
+                value.ToString(), 
+                new Font(FontFamily.GenericMonospace, 8), 
+                new SolidBrush(Color.White), 
+                new PointF(
+                    gridBaseX + (x * gridSpace) + gridSize / 3,
+                    gridBaseY + gridSize / 4));
+        }
+
+        private void btnOperate_Click(object sender, EventArgs e)
+        {
+            this.tbConsole.Clear();
+
+            if (this.tbQueryString.Text != "" || this.tbWindowSize.Text != "")
+            {
+                string data = this.tbQueryString.Text;
+                int windowSize = int.Parse(this.tbWindowSize.Text);
+
+                //카운트 배열 선언
+                int[] count = new int[windowSize];
+                for (int n = 0; n < windowSize; n++)
+                    count[n] = 0;
+
+                int[] fltcnt = new int[windowSize];
+                for (int n = 0; n < windowSize; n++)
+                    fltcnt[n] = 0;
+
+                int cnt = 0;
+                //마이그레이션 카운트
+                int[] numb = new int[data.Length];
+                for (int n = 0; n < data.Length; n++)
+                    numb[n] = 0;
+                int nu = 0;
+
+                /* initalize */
+                var window = new LRU(windowSize);
+                var window1 = new LFU(windowSize);
+                var window2 = new MFU(windowSize);
+                if (comboBox1.Text == "LRU")
+                {
+                    window = new LRU(windowSize);
+                }
+                else if(comboBox1.Text == "LFU")
+                {
+                    window1 = new LFU(windowSize);
+                }
+                else if (comboBox1.Text == "MFU")
+                {
+                    window2 = new MFU(windowSize);
+                }
+                    
+
+
+
+                foreach ( char element in data )
+                {
+
+
+                    if (comboBox1.Text == "LRU")
+                    {
+                        var status = window.Operate(element, ref count, ref cnt, ref numb, ref nu);
+                        this.tbConsole.Text += "DATA " + element + " is " +
+                        ((status == Page.STATUS.PAGEFAULT) ? "Page Fault" : status == Page.STATUS.MIGRATION ? "Migrated" : "Hit")
+                        + "\r\n";
+
+                    }
+                    else if (comboBox1.Text == "LFU")
+                    {
+                        var status = window1.Operate(element, ref count, ref cnt, ref numb, ref nu, ref fltcnt);
+                        this.tbConsole.Text += "DATA " + element + " is " +
+                        ((status == Page.STATUS.PAGEFAULT) ? "Page Fault" : status == Page.STATUS.MIGRATION ? "Migrated" : "Hit")
+                        + "\r\n";
+                    }
+                    else if (comboBox1.Text == "MFU")
+                    {
+                        var status = window2.Operate(element, ref count, ref cnt, ref numb, ref nu, ref fltcnt);
+                        this.tbConsole.Text += "DATA " + element + " is " +
+                        ((status == Page.STATUS.PAGEFAULT) ? "Page Fault" : status == Page.STATUS.MIGRATION ? "Migrated" : "Hit")
+                        + "\r\n";
+                    }
+                    
+                }
+                if (comboBox1.Text == "LRU")
+                    DrawBase(window, windowSize, data.Length, numb);
+                else if (comboBox1.Text == "LFU")
+                    DrawBase(window1, windowSize, data.Length, numb);
+                else if (comboBox1.Text == "MFU")
+                    DrawBase(window2, windowSize, data.Length, numb);
+                this.pbPlaceHolder.Refresh();
+
+                /* 차트 생성 */
+                chart1.Series.Clear();
+                Series resultChartContent = chart1.Series.Add("Statics");
+                resultChartContent.ChartType = SeriesChartType.Pie;
+                resultChartContent.IsVisibleInLegend = true;
+                if (comboBox1.Text == "LRU")
+                {
+                    resultChartContent.Points.AddXY("Hit", window.hit);
+                    resultChartContent.Points.AddXY("Page Fault", window.fault - window.migration);
+                    resultChartContent.Points.AddXY("Migrated", window.migration);
+                }
+                else if (comboBox1.Text == "LFU")
+                {
+                    resultChartContent.Points.AddXY("Hit", window1.hit);
+                    resultChartContent.Points.AddXY("Page Fault", window1.fault - window1.migration);
+                    resultChartContent.Points.AddXY("Migrated", window1.migration);
+                }
+                else if (comboBox1.Text == "MFU")
+                {
+                    resultChartContent.Points.AddXY("Hit", window2.hit);
+                    resultChartContent.Points.AddXY("Page Fault", window2.fault - window2.migration);
+                    resultChartContent.Points.AddXY("Migrated", window2.migration);
+                }
+                resultChartContent.Points[0].IsValueShownAsLabel = true;
+                resultChartContent.Points[1].IsValueShownAsLabel = true;
+                resultChartContent.Points[2].IsValueShownAsLabel = true;
+                if (comboBox1.Text == "LRU")
+                    this.lbPageFaultRatio.Text = Math.Round(((float)window.fault / (window.fault + window.hit)), 2) * 100 + "%";
+                else if (comboBox1.Text == "LFU")
+                    this.lbPageFaultRatio.Text = Math.Round(((float)window1.fault / (window1.fault + window1.hit)), 2) * 100 + "%";
+                else if (comboBox1.Text == "MFU")
+                    this.lbPageFaultRatio.Text = Math.Round(((float)window2.fault / (window2.fault + window2.hit)), 2) * 100 + "%";
+            }
+            else
+            {
+            }
+
+        }
+
+        private void pbPlaceHolder_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbWindowSize_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void tbWindowSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (!(Char.IsDigit(e.KeyChar)) && e.KeyChar != 8)
+                {
+                    e.Handled = true;
+                }
+        }
+
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            int count = rd.Next(5, 50);
+            StringBuilder sb = new StringBuilder();
+
+
+            for ( int i = 0; i < count; i++ )
+            {
+                sb.Append((char)rd.Next(65, 90));
+            }
+
+            this.tbQueryString.Text = sb.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            bResultImage.Save("./result.jpg");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Memory_Policy_Simulator/MFU.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Policy_Simulator
+{
+    class MFU
+    {
+        private int cursor;
+        public int p_frame_size;
+        public List<Page> frame_window;
+        public List<Page> pageHistory;
+
+        public int hit;
+        public int fault;
+        public int migration;
+
+        public MFU(int get_frame_size)
+        {
+            this.cursor = 0;
+            this.p_frame_size = get_frame_size;
+            this.frame_window = new List<Page>();
+            this.pageHistory = new List<Page>();
+        }
+
+        public Page.STATUS Operate(char data, ref int[] count, ref int cnt, ref int[] numb, ref int nu, ref int[] fltcnt)
+        {
+            Page newPage;
+
+            if (this.frame_window.Any<Page>(x => x.data == data))
+            {
+                newPage.pid = Page.CREATE_ID++;
+                newPage.data = data;
+                newPage.status = Page.STATUS.HIT;
+                this.hit++;
+                int i;
+
+                for (i = 0; i < this.frame_window.Count; i++)
+                {
+                    if (this.frame_window.ElementAt(i).data == data) break;
+                }
+
+                for (int n = 0; n < p_frame_size; n++)
+                    count[n]++;
+                count[i] = 0;
+                fltcnt[i]++;
+                newPage.loc = i + 1;
+
+                nu++;
+            }
+            else
+            {
+                newPage.pid = Page.CREATE_ID++;
+                newPage.data = data;
+
+                if (frame_window.Count >= p_frame_size)
+                {
+                    newPage.status = Page.STATUS.MIGRATION;
+                    
+                    int max = count[0];
+                    int maxloc = 0;
+                    for (int n = 1; n < p_frame_size; n++)
+                    {
+                        if (count[n] > max)
+                        {
+                            max = count[n];
+                            maxloc = n;
+                        }
+                    }
+
+                    int min = fltcnt[0];
+                    int minloc = 0;
+                    for (int n = 1; n < p_frame_size; n++)
+                    {
+                        if (fltcnt[n] > min)
+                        {
+                            min = fltcnt[n];
+                            minloc = n;
+                        }
+                    }
+                    int fltcount = 0;
+                    for (int n = 0; n < p_frame_size; n++)
+                        if (min == fltcnt[n])
+                            fltcount++;
+                    if(fltcount > 1)
+                    {
+                        int[] arr = new int[fltcount];
+                        fltcount = 0;
+                        for (int n = 0; n < p_frame_size; n++)
+                            if (fltcnt[n] == min)
+                                arr[fltcount++] = n;
+                        fltcount = count[arr[0]];
+                        maxloc = arr[0];
+                        for (int n = 1; n < arr.Length; n++)
+                        {
+                            if (count[arr[n]] > fltcount)
+                            {
+                                fltcount = count[arr[n]];
+                                maxloc = arr[n];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        maxloc = minloc;
+                    }
+
+                        this.frame_window.RemoveAt(maxloc);
+                    for (int n = 0; n < p_frame_size; n++)
+                        count[n]++;
+                    count[maxloc] = 0;
+                    fltcnt[maxloc] = 1;
+                    cursor = maxloc + 1;
+                    this.migration++;
+                    this.fault++;
+
+                    newPage.loc = cursor;
+                    frame_window.Insert(maxloc, newPage);
+
+                    numb[nu] = cursor - 1;
+                    nu++;
+                }
+                else
+                {
+                    newPage.status = Page.STATUS.PAGEFAULT;
+                    cursor++;
+                    this.fault++;
+                    for (int n = 0; n < p_frame_size; n++)
+                    {
+                        if (count[n] != 0)
+                            count[n]++;
+                    }
+                    count[cnt] = 1;
+                    fltcnt[cnt]++;
+                    cnt++;
+
+                    newPage.loc = cursor;
+                    frame_window.Add(newPage);
+
+                    nu++;
+                }
+
+            }
+            pageHistory.Add(newPage);
+
+            return newPage.status;
+        }
+
+        public List<Page> GetPageInfo(Page.STATUS status)
+        {
+            List<Page> pages = new List<Page>();
+
+            foreach (Page page in pageHistory)
+            {
+                if (page.status == status)
+                {
+                    pages.Add(page);
+                }
+            }
+
+            return pages;
+        }
+
+    }
+
+
+}
